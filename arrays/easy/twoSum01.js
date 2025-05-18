f17a85d8b7015666f78980d816d7f4f57c461deb
Deeksha <sharmadeekksha@gmail.com>
2025-05-18 20:54:33 +0530

    solved 01 leetcode by HashMap

---
+++ arrays/easy/twoSum01.js
@@ -0,0 +1,36 @@
+// Leetcode 1. Two Sum
+// Difficulty: Easy
+// Approach: HashMap 
+
+// Intuition:
+// Given an array of integers, we need to find two numbers such that they add up to a specific target.
+// We can use a hashmap to solve this problem.
+// We will iterate through the array and for each element, we will check if the target - element exists in the hashmap.
+// If it exists, we have found the two numbers and we can return their indices.
+// If it does not exist, we will add the element to the hashmap along with its index.
+
+// Complexity:
+// Time complexity: O(n)
+// Space complexity: O(n)
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let map = new Map();
+   for(let i=0; i<nums.length; i++){
+    let key = target-nums[i];
+    if(map.has(key)){
+        return [map.get(key), i];
+    }
+    map.set(nums[i],i);
+   }
+}
+console.log(twoSum([2,7,11,15],9)); // [0,1]
+console.log(twoSum([3,2,4],6)); // [1,2]
