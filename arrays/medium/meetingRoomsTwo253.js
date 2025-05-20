d5fe5a15cbece33e09b42229437f680db1f7ea93
Deeksha <sharmadeekksha@gmail.com>
2025-05-20 13:06:11 +0530

    Brute force approach # 253 leetcode

--- arrays/medium/meetingRoomsTwo253.js
+++ arrays/medium/meetingRoomsTwo253.js
@@ -1,34 +1,74 @@
 // Leetcode 253. Meeting Rooms II
 // Difficulty: Medium
-// Approach: Sorting + Intervals 
+// Approach: Sorting + Intervals
 
 // Intuition:
+// Given an array of meeting time intervals consisting of start and end times, we need to determine the minimum number of conference rooms required to hold all meetings.
 
+//Brute Force approach:
+// 1. Create a new Arr rooms which stores the end time of each meeting.
+// 2. For each meeting, check if it can be placed in an existing room. by comparing its start time with rooms[] end times.
+// 3. If it can be placed, update the end time of that room in rooms[].
+// 4. If it cannot be placed, add a new room to rooms[].
+// 5. The length of rooms[] will be the minimum number of conference rooms required.
+// 6. Return the length of rooms[].
+// 7. If intervals is empty, return 0.
 
 // Complexity:
+// Time - O(n^2) for brute force
+// Space - O(n) for brute force
 
+// Brute force approach:
+var minMeetingRoomsBF = function (intervals) {
+  if (!intervals || intervals.length === 0) return 0;
 
-var minMeetingRooms = function(intervals) {
-//     if (!intervals || intervals.length === 0) return 0;
-//     // let min=1;
+  let rooms = [];
 
-//     intervals.sort((a,b)=> a[0]-b[0]);
-//     let merged=[intervals[0]];
+  for (let i = 0; i < intervals.length; i++) {
+    let placed = false;
 
-//     for(let i=1; i<intervals.length ; i++){
-//         let last = merged[merged.length-1];
-//         let curr = intervals[i];
+    // Check if the meeting can be placed in an existing room
+    for (let j = 0; j < rooms.length; j++) {
+      if (intervals[i][0] >= rooms[j]) {
+        rooms[j] = intervals[i][1];
+        placed = true;
+        break;
+      }
+    }
+    // If it cannot be placed in any existing room, add a new room
+    if (!placed) {
+      rooms.push(intervals[i][1]);
+    }
+  }
+  return rooms.length;
+};
 
-//         if(last[1]>curr[0]){
-//             // min++;
-//             merged.push(curr);
-//         }
-//     }
-//     // return min;
-// return merged.length;
-}
-
-console.log(minMeetingRooms([[0,30],[5,10],[15,20]])); // 2
-console.log(minMeetingRooms([[7,10],[2,4]])); // 1
-console.log(minMeetingRooms([[1,2],[2,3],[3,4],[4,5]])); // 1
-console.log(minMeetingRooms([[0,30],[5,10],[15,20],[20,25]])); // 2
+console.log(
+  minMeetingRoomsBF([
+    [0, 30],
+    [5, 10],
+    [15, 20],
+  ])
+); // 2
+console.log(
+  minMeetingRoomsBF([
+    [7, 10],
+    [2, 4],
+  ])
+); // 1
+console.log(
+  minMeetingRoomsBF([
+    [1, 2],
+    [2, 3],
+    [3, 4],
+    [4, 5],
+  ])
+); // 1
+console.log(
+  minMeetingRoomsBF([
+    [0, 30],
+    [5, 10],
+    [15, 20],
+    [20, 25],
+  ])
+); // 2
