927f8a5abbd9b6faa10954881e816595e1a62835
Deeksha <sharmadeekksha@gmail.com>
2025-05-26 21:25:27 +0530

    solved 27 # leetcode Remove elements

---
+++ arrays/easy/removeElement27.js
@@ -0,0 +1,38 @@
+// Leetcode 27. Remove Element
+// Difficulty: Easy
+// Approach: Two Pointers
+
+// Intuition:
+// We can use two pointers to keep track of the position to insert the next element that is not equal to val.
+// The first pointer will iterate through the array, and the second pointer will keep track of the position to insert the next element that is not equal to val.
+// we return number of elements that are not equal to val.
+// This way, we can modify the array in place and return the new length of the array without the elements equal to val.
+
+// Complexity:
+// Time Complexity: O(n), where n is the length of the array.
+// Space Complexity: O(1), since we are modifying the array in place and not using any extra space.
+
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let i=0;
+    let j=0;
+    let pos=false;
+    while(j<nums.length){
+        if(nums[j] == val) {
+           pos=true;
+        }else
+        {
+            if(pos) nums[i]=nums[j];
+            i++;}
+        j++;
+        
+    }
+    return i;
+};
+
+console.log(removeElement([3,2,2,3], 3)); // Output: 2
+console.log(removeElement([0,1,2,2,3,0,4,2], 2)); // Output: 5
