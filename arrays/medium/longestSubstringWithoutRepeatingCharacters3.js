4fbfe2ea93bf84e9bde0b67d067a86592f8e52c9
Deeksha <sharmadeekksha@gmail.com>
2025-05-29 14:49:12 +0530

    updated approach for #3 leetcode

--- arrays/medium/longestSubstringWithoutRepeatingCharacters3.js
+++ arrays/medium/longestSubstringWithoutRepeatingCharacters3.js
@@ -1,7 +1,15 @@
-// LeetCode #3: Longest Substring Without Repeating Characters 
+// LeetCode #3: Longest Substring Without Repeating Characters
 // Difficulty: Medium
 // Category: Arrays
 // Approach: Sliding Window & Sets
+
+// Intuition:
+// we will iterate through string.
+// one pointer left shall keep track of start of set.
+// we will capture all unique characters of string .
+// if any char repeates itself, all elements from left of map shall be deleted until given char gets deleted. this will result in unique substring in set.
+// max size of substring in the set at a given time will be captured.
+// max size of set is returned.
 
 // # Time Complexity
 // O(n) for the two pointers
@@ -9,20 +17,19 @@
 // # Space Complexity
 // O(n) for the set
 
-
-var lengthOfLongestSubstring = function(s) {
-        let set = new Set();
-        let j=0;
-        let maxLen=0;
-        for(let i=0;i<s.length;i++){
-           while(set.has(s[i])){
-            set.delete(s[j]);
-            j++;
-           } 
-           set.add(s[i]);
-           maxLen=Math.max(maxLen,i-j+1);
-        }
-        return maxLen;
-    };
+var lengthOfLongestSubstring = function (s) {
+  let set = new Set();
+  let left = 0;
+  let max = 0;
+  for (let i = 0; i < s.length; i++) {
+    while (set.has(s[i])) {
+      set.delete(s[left]);
+      left++;
+    }
+    set.add(s[i]);
+    max = Math.max(max, set.size);
+  }
+  return max;
+};
 
 console.log(lengthOfLongestSubstring("abcabcbb"));
