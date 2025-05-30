fe67fd3a8b36fabee1faf70ab6afd2d84b870da5
Deeksha <sharmadeekksha@gmail.com>
2025-05-30 16:51:44 +0530

    updated approach of #1876

--- arrays/easy/substringsOfSize3WithDistinctChar1876.js
+++ arrays/easy/substringsOfSize3WithDistinctChar1876.js
@@ -3,45 +3,51 @@
 // Category: Arrays
 // Approach: Two Pointers & sets
 
-//Complexity: O(n) & Space: O(1)
+
+// Appraoch :
+// we will iterate over the String & will add every new element of string to a set.
+// We will check if set already doesnt have same element present, if yes, we will delete all elements from start of till element present in set.
+//we will track start of set alongside.
+// when set size will be greater than 3, count will increment.
+// return count.
+
+
+
+//Complexity: O(n) & Space: O(n)
 /**
  * @param {string} s
  * @return {number}
  */
 var countGoodSubstrings = function(s) {
     let set = new Set();
-    let count=0;let l=0;
+    let count=0;
+    let left=0;
     for(let i=0;i<s.length;i++){
         while(set.has(s[i])){
-           set.delete(s[l]);
-           l++;
+           set.delete(s[left]);
+           left++;
         }
         set.add(s[i]);
-        if(set.size>3){
-         set.delete(s[l]);
-         l++;
-        }
-        if(set.size === (3)){
-        count++;}    
+        if(set.size>=3){count++;}    
     }
     return count;
 };
 
 console.log(countGoodSubstrings("xyzzaz"));
-
+console.log(countGoodSubstrings("aababcabc"));
 
 // #More Optimum & easier Approach
 //Complexity: O(n) & Space: O(1)
 // # Code
 // javascript []
-var countGoodSubstrings = function(s) {
-    let count = 0;
-    for (let i = 0; i <= s.length - 3; i++) {
-        let a = s[i], b = s[i+1], c = s[i+2];
-        if (a !== b && b !== c && a !== c) {
-            count++;
-        }
-    }
-    return count;
-};
-console.log(countGoodSubstrings("xyzzaz"));
+// var countGoodSubstrings = function(s) {
+//     let count = 0;
+//     for (let i = 0; i <= s.length - 3; i++) {
+//         let a = s[i], b = s[i+1], c = s[i+2];
+//         if (a !== b && b !== c && a !== c) {
+//             count++;
+//         }
+//     }
+//     return count;
+// };
+// console.log(countGoodSubstrings("xyzzaz"));
---
+++ arrays/easy/tempCodeRunnerFile.js
@@ -0,0 +1,11 @@
+tGoodSubstrings = function(s) {
+//     let count = 0;
+//     for (let i = 0; i <= s.length - 3; i++) {
+//         let a = s[i], b = s[i+1], c = s[i+2];
+//         if (a !== b && b !== c && a !== c) {
+//             count++;
+//         }
+//     }
+//     return count;
+// };
+// console.lo
