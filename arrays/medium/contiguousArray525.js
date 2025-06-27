6b853d5bb4f0f4f123224fbad88115f415f2da4c
Deeksha <sharmadeekksha@gmail.com>
2025-06-27 14:29:49 +0530

    update: logic of #525 leetcode

--- arrays/medium/contiguousArray525.js
+++ arrays/medium/contiguousArray525.js
@@ -1,8 +1,8 @@
 //Leetcode 525. Contiguous Array
 // Difficulty: Medium
-// Approach: Prefix sum & HashMap
+// Implementation: Prefix sum & HashMap
 
-// Intuition:
+// Approach:
 // Given an array of 0s and 1s, we need to find the longest contiguous subarray with equal number of 0s and 1s.
 // We can replace 0 with -1, so the problem becomes finding the longest contiguous subarray with sum 0.
 // now, Problem becomes easy when all 0 are replaced with -1
@@ -12,27 +12,27 @@
 // If we find the same prefix sum again, it means the subarray between the two indices has sum 0.
 // We can calculate the length of this subarray and update the maximum length if it is greater than the previous maximum length.
 
-
-
-
 /**
  * @param {number[]} nums
  * @return {number}
  */
-var findMaxLength = function(nums) {
-    let map=new Map();
-    map.set(0,-1);
-    let sum=0;
-    let maxLen=0;
+var findMaxLength = function (nums) {
+  let map = new Map();
+  map.set(0, -1);
+  let sum = 0;
+  let maxLen = 0;
 
-    for(let i=0;i<nums.length;i++){
-        sum += nums[i]===0? -1: 1;
-        if(map.has(sum)) {
-            maxLen = Math.max(maxLen, i - map.get(sum))
-        }
-        map.set(sum,i);
+  for (let i = 0; i < nums.length; i++) {
+    sum = nums[i] === 0 ? sum - 1 : sum + 1;
+    if (map.has(sum)) {
+      maxLen = Math.max(maxLen, i - map.get(sum));
+    } else {
+      map.set(sum, i);
     }
-    return maxLen;
+  }
+  return maxLen;
 };
 
-console.log(findMaxLength([0,1,0,0,1,1,0,1,0,1])); //4
+console.log(findMaxLength([0, 1, 0, 0, 1, 1, 0, 1, 0, 1])); //10
+console.log(findMaxLength([0, 1, 0, 1, 0, 1])); //6
+console.log("", findMaxLength([0, 1, 0])); //2
