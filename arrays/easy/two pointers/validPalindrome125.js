96304f5c47069b227e715381ad3ab059a96d4eb7
Deeksha <sharmadeekksha@gmail.com>
2025-11-11 15:24:09 +0530

    update: problem statement

--- arrays/easy/two pointers/validPalindrome125.js
+++ arrays/easy/two pointers/validPalindrome125.js
@@ -9,7 +9,7 @@
 
 // Intuition:
 // We will need two pointers & check string from start & end. Only alphanumeric char must be checked & non-alphanumeric ones must be skipped.
-// if two pointers meet each other, string is a valid palindrome. 
+// if two pointers meet each other, string is a valid palindrome.
 // If not, return false.
 
 // Approach:
@@ -18,43 +18,45 @@
 //      - We will check if both pointers are pointing to a valid alphanumeric char.
 //      - If yes, we will compare if they are equal or not. If not, return false.
 //      - If no, we shall skip the pointer untill it points to a valid alphanumeric char.
-// When pointers meet each other, return true; 
+// When pointers meet each other, return true;
+
+// Complexity : Time: O(n) Space: O(1)
 
 // Code:
-var isPalindrome = function(s) {
-    s=s.toLowerCase();
-    let left=0;
-    let right=s.length-1;
-    
-    while(left<right){
-        if(!isAlphaNum(s[left])){
-            left++;
-            continue;
-        }
-        if(!isAlphaNum(s[right])){
-            right--;
-            continue;
-        }
-        if(s[left]!==s[right]){
-            return false;            
-            } 
+var isPalindrome = function (s) {
+  s = s.toLowerCase();
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    if (!isAlphaNum(s[left])) {
+      left++;
+      continue; // to bring incremented left element under conditional check
+    }
+    if (!isAlphaNum(s[right])) {
+      right--;
+      continue;
+    }
+    if (s[left] !== s[right]) {
+      return false;
+    }
     left++;
     right--;
-    }
-     return true;
+  }
+  return true;
 };
 
 //use const for helper functions if we arent reassigning
- const isAlphaNum=(char)=>{
-        let regex = /^[a-z0-9]$/;
-        return regex.test(char);
-}
+const isAlphaNum = (char) => {
+  let regex = /^[a-z0-9]$/;
+  return regex.test(char);
+};
 
-// Complexity : Time- O(n) Space: O(1)
+// ^ - denotes start of string.
 
+// --------------------------------
 
-// -----------------------------------
-//2nd solution- Concise 
+// 2nd solution- Concised form
 
 //Intuition:
 // We can use the two pointers approach to solve this problem without creating a new string.
@@ -68,23 +70,21 @@ var isPalindrome = function(s) {
 // Time Complexity: O(n), where n is the length of the input string.
 // Space Complexity: O(1), since we are not using any extra space.
 
-var isPalindromeOptimized = function(s) {
-    let left=0;
-    let right=s.length-1;
-    while(left<right){
-        while (left < right && !isAlphaNum(s[left])) left++;
-        while (left < right && !isAlphaNum(s[right])) right--;
+var isPalindromeOptimized = function (s) {
+  let left = 0;
+  let right = s.length - 1;
+  while (left < right) {
+    while (left < right && !isAlphaNum2(s[left])) left++;
+    while (left < right && !isAlphaNum2(s[right])) right--;
 
-        if (s[left++].toLowerCase() !== s[right--].toLowerCase()) return false;
-    }
-    return true;
-}
+    if (s[left++].toLowerCase() !== s[right--].toLowerCase()) return false;
+  }
+  return true;
+};
 
-//Used in 1st Solution
-// const isAlphaNum = (char) => {
+// const isAlphaNum2 = (char) => {
 //  return /[a-z0-9]/.test(char);
 // }
-
 
 //-----------------------------------
 
@@ -103,14 +103,14 @@ var isPalindromeOptimized = function(s) {
 // Time Complexity: O(n), where n is the length of the input string.
 // Space Complexity: O(n), since we are creating new string after removing non-alphanumeric characters.
 
-var isPalindrome = function(s) {
-    s=(s.replace(/[^a-zA-Z0-9]/g,'').toLowerCase());
-    let left=0;
-    let right=s.length-1;
-    while(left<right){
-        if(s[left++]!==s[right--]) return false;
-    }
-    return true;
+var isPalindrome = function (s) {
+  s = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+  let left = 0;
+  let right = s.length - 1;
+  while (left < right) {
+    if (s[left++] !== s[right--]) return false;
+  }
+  return true;
 };
 
 console.log(isPalindrome("race a car")); // false
