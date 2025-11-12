25da6866206dd738bdd643197d080ba97598237a
Deeksha <sharmadeekksha@gmail.com>
2025-11-12 14:29:38 +0530

    chore: added edge cases

--- arrays/easy/moveZeroes283.js
+++ arrays/easy/moveZeroes283.js
@@ -18,18 +18,47 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var moveZeroes = function(nums) {
-    let pos=0;
-    let i=0;
-    while(i<nums.length){
-        if(nums[i] !== 0 ){
-            [nums[pos],nums[i]]=[nums[i],nums[pos]];
-            pos++;
-            }
-     i++;    
+var moveZeroes = function (nums) {
+  let pos = 0;
+  let i = 0;
+  while (i < nums.length) {
+    if (nums[i] !== 0) {
+      [nums[pos], nums[i]] = [nums[i], nums[pos]];
+      pos++;
+    }
+    i++;
+  }
+  return nums;
 };
-return nums;
-}
 
-console.log(moveZeroes([0,1,0,3,12])); // [1,3,12,0,0]
-console.log(moveZeroes([0])); // [0]
+// 🧪 Edge Case Tests
+
+console.log("Mixed zeros and numbers:", moveZeroes([0, 1, 0, 3, 12]));
+// [1,3,12,0,0]
+
+console.log("All zeros:", moveZeroes([0, 0, 0, 0]));
+// [0,0,0,0]
+
+console.log("No zeros:", moveZeroes([1, 2, 3, 4]));
+// [1,2,3,4]
+
+console.log("Zeros at end already:", moveZeroes([1, 2, 3, 0, 0]));
+// [1,2,3,0,0]
+
+console.log("Zeros at start:", moveZeroes([0, 0, 1, 2, 3]));
+// [1,2,3,0,0]
+
+console.log("Single element zero:", moveZeroes([0]));
+// [0]
+
+console.log("Single element non-zero:", moveZeroes([5]));
+// [5]
+
+console.log("Alternating zeros:", moveZeroes([0, 5, 0, 4, 0, 3, 0, 2]));
+// [5,4,3,2,0,0,0,0]
+
+console.log("Negative numbers and zeros:", moveZeroes([0, -1, 0, -2, 3]));
+// [-1,-2,3,0,0]
+
+console.log("Zeros scattered:", moveZeroes([4, 0, 5, 0, 0, 3, 0, 2, 0]));
+// [4,5,3,2,0,0,0,0,0]
