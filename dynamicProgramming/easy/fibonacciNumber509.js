567eefc7fbc783ac7974c45b3f322b649be16056
Deeksha <sharmadeekksha@gmail.com>
2026-05-19 21:44:55 +0530

    Update fibonacciNumber509.js

--- dynamicProgramming/easy/fibonacciNumber509.js
+++ dynamicProgramming/easy/fibonacciNumber509.js
@@ -3,3 +3,16 @@ var fib = function(n) {
     return fib(n-1)+fib(n-2);    
 }; 
 
+//--
+
+var fib = function(n) {
+    if(n === 0 || n === 1) return n;
+
+    let a =0, b=1, res =0;
+    for(i=2;i<=n;i++) {
+        res = a+b; 
+        a=b;
+        b= res;
+    }
+    return res;
+};
