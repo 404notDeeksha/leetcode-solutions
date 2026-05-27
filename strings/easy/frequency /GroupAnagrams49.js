3114305dae828bccbe39f2baeff0ec2c0d536364
Deeksha <sharmadeekksha@gmail.com>
2026-05-27 11:35:53 +0530

    remove solution difficult to comprehend

--- strings/easy/frequency /GroupAnagrams49.js
+++ strings/easy/frequency /GroupAnagrams49.js
@@ -16,28 +16,3 @@ var groupAnagrams = function(strs) {
   
   };
     
-// --------------
-
-  var groupAnagrams = function(strs) {
-    const map = {};
-
-    for (let word of strs) {
-        // 26-length frequency array
-        let freq = new Array(26).fill(0);
-
-        for (let ch of word) {
-            freq[ch.charCodeAt(0) - 97]++;
-        }
-
-        // Use frequency signature as key
-        let key = freq.join("#");
-
-        if (!map[key]) {
-            map[key] = [];
-        }
-
-        map[key].push(word);
-    }
-
-    return Object.values(map);
-};
