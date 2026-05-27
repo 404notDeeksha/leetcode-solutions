06ad8ea28f8bcb0af7bb7e95e23c74816bdeac57
Deeksha <sharmadeekksha@gmail.com>
2026-05-27 11:36:44 +0530

    add faster version of same code

--- strings/easy/frequency /GroupAnagrams49.js
+++ strings/easy/frequency /GroupAnagrams49.js
@@ -15,4 +15,20 @@ var groupAnagrams = function(strs) {
     return [...map.values()];
   
   };
-    
+// or
+ 
+var groupAnagrams = function(strs) {
+    let map = new Map();
+
+    for(let str of strs){
+        let newStr = str.split("").sort().join("");
+
+        if(!map.has(newStr)){
+            map.set(newStr,[str]);
+        }else{
+            map.get(newStr).push(str);
+        }
+        
+    }
+    return [...map.values()]
+};
