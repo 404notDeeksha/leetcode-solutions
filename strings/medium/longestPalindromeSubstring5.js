e8cae43bcedf76e902cfe88702ce8547080e2ddf
Deeksha <sharmadeekksha@gmail.com>
2026-07-29 23:28:07 +0530

    add checks to verify palindrome

--- strings/medium/longestPalindromeSubstring5.js
+++ strings/medium/longestPalindromeSubstring5.js
@@ -14,5 +14,21 @@ var longestPalindrome = function(s) {
         return s.slice(left + 1, right);
     }
 
+    for (let i = 0; i < s.length; i++) {
+        // Odd length palindrome
+        let odd = expand(i, i);
+
+        // Even length palindrome
+        let even = expand(i, i + 1);
+
+        if (odd.length > longest.length) {
+            longest = odd;
+        }
+
+        if (even.length > longest.length) {
+            longest = even;
+        }
+    }
+
     return longest;
 };
