ed2d40a925bda12de2b764494853ac87a5038bac
Deeksha <sharmadeekksha@gmail.com>
2026-07-29 23:27:37 +0530

    add approach to catch longest palindrome

--- strings/medium/longestPalindromeSubstring5.js
+++ strings/medium/longestPalindromeSubstring5.js
@@ -1,4 +1,18 @@
 var longestPalindrome = function(s) {
     let longest = "";
+
+    function expand(left, right) {
+        while (
+            left >= 0 &&
+            right < s.length &&
+            s[left] === s[right]
+        ) {
+            left--;
+            right++;
+        }
+
+        return s.slice(left + 1, right);
+    }
+
     return longest;
 };
